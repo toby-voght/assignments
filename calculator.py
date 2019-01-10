cd6999447e29f823caa92158b4c9b0ff62dfa3e1
Toby Voght <toby_voght@student.hks.harvard.edu>
2019-01-10 09:07:09 -0500

    print outputs of calculator functions

--- calculator.py
+++ calculator.py
@@ -14,3 +14,19 @@ def subtract(a,b):
 def divide(a,b):
 	return a/b
 	
+#add outputs
+print("This is the calculator function multiplying 2 and 3")
+x = multiply(2,3)
+print(x)
+
+print("This is the calculator function adding 3 and 4")
+x = add(3,4)
+print(x)
+
+print("This is the calculator function subtracting 4 and 5")
+x = subtract(4,5)
+print(x)
+
+print("This is the calculator function dividing 5 and 6")
+x = divide(5,6)
+print(x)
