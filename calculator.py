6434438966eef61558b258190d212863cf584c0d
Toby Voght <toby_voght@student.hks.harvard.edu>
2019-01-10 09:01:48 -0500

    add functions to calculator

---
+++ calculator.py
@@ -0,0 +1,16 @@
+#create multiply function
+def multiply(a,b):
+	return a*b
+
+#create add function
+def add(a,b):
+	return a+b
+
+#create subtract function
+def subtract(a,b):
+	return a-b
+
+#create divide function
+def divide(a,b):
+	return a/b
+	
