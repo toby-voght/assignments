367996003884334b36394d5cf2228a99e906e760
Toby Voght <toby_voght@student.hks.harvard.edu>
2019-01-10 09:32:26 -0500

    add print outputs of calculator function

--- calculator.js
+++ calculator.js
@@ -13,3 +13,19 @@ function subtract(a,b) {
 function divide(a,b) {
 	return a/b
 }
+
+console.log('This is the calculator function multiplying 5 and 6')
+var x = multiply(5,6)
+console.log(x)
+
+console.log('This is the calculator function adding 4 and 5')
+var x = add(4,5)
+console.log(x)
+
+console.log('This is the calculator function subtracting 4 from 3')
+var x = subtract(3,4)
+console.log(x)
+
+console.log('This is the calculator function dividing 2 by 3')
+var x = divide(2,3)
+console.log(x)
